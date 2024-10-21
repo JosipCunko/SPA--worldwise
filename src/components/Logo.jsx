ec6667841ed00dfde5610fea426864f30209d44d
JosipCunko <ultrabrzitranzijent@gmail.com>
2024-10-21 15:45:57 +0200

    Responsive

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,16 @@ import styles from "./Logo.module.css";
 function Logo() {
   return (
     <Link to="/">
-      <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
+      <picture>
+        <source media="(max-width: 500px)" srcSet="/icon.png" />
+        <source media="(min-width: 501px)" srcSet="/logo.png" />
+        <img
+          src="/logo.png" // fallback for older browsers
+          alt="WorldWise logo"
+          className={styles.logo}
+        />
+      </picture>
     </Link>
   );
 }
-
 export default Logo;
